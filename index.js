4ce5aee1d599fdfb3c1f86401c0602502864526a
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2021-04-14 21:43:41 +0200

    prettier

--- index.js
+++ index.js
@@ -1,98 +1,96 @@
-var instance_skel = require('../../instance_skel');
-var tcp           = require('../../tcp');
+var instance_skel = require('../../instance_skel')
+var tcp = require('../../tcp')
 
-var debug;
-var log;
+var debug
+var log
 
 function instance(system, id, config) {
-	var self = this;
+	var self = this
 
 	// super-constructor
-	instance_skel.apply(this, arguments);
-	
+	instance_skel.apply(this, arguments)
+
 	// export actions
-	self.actions(); 
-	
+	self.actions()
+
 	// presets
 	// self.init_presets();
 
-	return self;
+	return self
 }
 
 instance.prototype.updateConfig = function (config) {
-	var self = this;
+	var self = this
 	// self.init_presets();
 
 	if (self.socket !== undefined) {
-		self.socket.destroy();
-		delete self.socket;
+		self.socket.destroy()
+		delete self.socket
 	}
 
-	self.config = config;
-	self.init_tcp();
-	
-};
+	self.config = config
+	self.init_tcp()
+}
 
 instance.prototype.init = function () {
-	var self = this;
+	var self = this
 
-	debug = self.debug;
-	log = self.log;
-	
+	debug = self.debug
+	log = self.log
+
 	// self.init_presets();
-	self.init_tcp();
+	self.init_tcp()
+}
 
-};
-
-instance.prototype.init_tcp = function() {
-	var self = this;
+instance.prototype.init_tcp = function () {
+	var self = this
 
 	if (self.socket !== undefined) {
-		self.socket.destroy();
-		delete self.socket;
+		self.socket.destroy()
+		delete self.socket
 	}
 
-	self.status(self.STATE_WARNING, 'Connecting');
+	self.status(self.STATE_WARNING, 'Connecting')
 
 	if (self.config.host) {
-		self.socket = new tcp(self.config.host, self.config.port);
+		self.socket = new tcp(self.config.host, self.config.port)
 
 		self.socket.on('status_change', function (status, message) {
-			self.status(status, message);
-		});
-
-		self.socket.on('error', function (err) {
-			debug("Network error", err);
-			self.status(self.STATE_ERROR, err);
-			self.log('error',"Network error: " + err.message);
-		});
-
-		self.socket.on('connect', function () {
-			self.status(self.STATE_OK);
-			debug("Connected");
+			self.status(status, message)
 		})
 
-		self.socket.on('data', function (data) {});
+		self.socket.on('error', function (err) {
+			debug('Network error', err)
+			self.status(self.STATE_ERROR, err)
+			self.log('error', 'Network error: ' + err.message)
+		})
+
+		self.socket.on('connect', function () {
+			self.status(self.STATE_OK)
+			debug('Connected')
+		})
+
+		self.socket.on('data', function (data) {})
 	}
-};
+}
 
 // Return config fields for web config
 instance.prototype.config_fields = function () {
-	var self = this;
+	var self = this
 	return [
 		{
 			type: 'text',
 			id: 'info',
 			width: 12,
 			label: 'Information',
-			value: 'This module is for NEXO NXAMP amplifiers'
+			value: 'This module is for NEXO NXAMP amplifiers',
 		},
 		{
 			type: 'textinput',
 			id: 'host',
 			label: 'Device IP',
 			width: 6,
-			regex: self.REGEX_IP
+			regex: self.REGEX_IP,
 		},
 		{
 			type: 'textinput',
@@ -100,34 +98,34 @@ instance.prototype.config_fields = function () {
 			label: 'Control port',
 			width: 6,
 			default: '49280',
-			regex: self.REGEX_PORT
-		}
+			regex: self.REGEX_PORT,
+		},
 	]
-};
+}
 
 // When module gets deleted
 instance.prototype.destroy = function () {
-	var self = this;
+	var self = this
 
 	if (self.socket !== undefined) {
-		self.socket.destroy();
+		self.socket.destroy()
 	}
 
-	debug("destroy", self.id);;
-};
+	debug('destroy', self.id)
+}
 
 instance.prototype.init_presets = function () {
-	var self = this;
-	var presets = [];
-	
-	self.setPresetDefinitions(presets);
+	var self = this
+	var presets = []
+
+	self.setPresetDefinitions(presets)
 }
 
 instance.prototype.actions = function (system) {
-	var self = this;
+	var self = this
 
 	self.setActions({
-		'power': {
+		power: {
 			label: 'Power Control',
 			options: [
 				{
@@ -136,13 +134,13 @@ instance.prototype.actions = function (system) {
 					id: 'power',
 					default: '0',
 					choices: [
-						{ id: '1', label: 'On'}, 
-						{ id: '0', label: 'Off'}
-					]
-				}
-			]
+						{ id: '1', label: 'On' },
+						{ id: '0', label: 'Off' },
+					],
+				},
+			],
 		},
-		'overMute': {
+		overMute: {
 			label: 'Overmute',
 			options: [
 				{
@@ -151,13 +149,13 @@ instance.prototype.actions = function (system) {
 					id: 'overmute',
 					default: '0',
 					choices: [
-						{ id: '1', label: 'On'}, 
-						{ id: '0', label: 'Off'}
-					]
-				}
-			]
+						{ id: '1', label: 'On' },
+						{ id: '0', label: 'Off' },
+					],
+				},
+			],
 		},
-		'channelMute': {
+		channelMute: {
 			label: 'Channel Mute',
 			options: [
 				{
@@ -166,87 +164,84 @@ instance.prototype.actions = function (system) {
 					id: 'channelMute',
 					min: 1,
 					max: 4,
-					default: 1
-				},			
+					default: 1,
+				},
 				{
 					type: 'dropdown',
 					label: 'Mute',
 					id: 'muteState',
 					default: '0',
 					choices: [
-						{ id: '1', label: 'On'}, 
-						{ id: '0', label: 'Off'}
-					]
-				}				
-			]
+						{ id: '1', label: 'On' },
+						{ id: '0', label: 'Off' },
+					],
+				},
+			],
 		},
-	});
-};
+	})
+}
 
 instance.prototype.action = function (action) {
-	var self = this;
-	const opt = action.options;
-	console.log('action: ' + action.action);
-	
+	var self = this
+	const opt = action.options
+	console.log('action: ' + action.action)
+
 	switch (action.action) {
-		
 		case 'power': {
-			var powerCmd = 'set AMP:Power 0 0 ' + opt.power;
-			console.log(powerCmd);
-			self.log('debug',powerCmd)
-			self.sendCmd(powerCmd);
-			break;		
+			var powerCmd = 'set AMP:Power 0 0 ' + opt.power
+			console.log(powerCmd)
+			self.log('debug', powerCmd)
+			self.sendCmd(powerCmd)
+			break
 		}
 		case 'overMute': {
-			var overmuteCmd = 'set AMP:Overmute 0 0 ' + opt.overmute;
-			console.log('debug',overmuteCmd);
-			self.sendCmd(overmuteCmd);
-			break;	
-		}	
+			var overmuteCmd = 'set AMP:Overmute 0 0 ' + opt.overmute
+			console.log('debug', overmuteCmd)
+			self.sendCmd(overmuteCmd)
+			break
+		}
 		case 'channelMute': {
-			var muteCmd = 'set AMP:Ch/Mute ' + (opt.channelMute - 1) + ' 0 ' + Number(opt.muteState);
-			console.log(muteCmd);
-			self.log('debug',muteCmd);
-			self.sendCmd(muteCmd);
-			break;	
+			var muteCmd = 'set AMP:Ch/Mute ' + (opt.channelMute - 1) + ' 0 ' + Number(opt.muteState)
+			console.log(muteCmd)
+			self.log('debug', muteCmd)
+			self.sendCmd(muteCmd)
+			break
 		}
 		default:
-			break;
+			break
 	}
+}
 
-};
+instance.prototype.sendCmd = function (cmdStr) {
+	var self = this
+	var cmd
+	var end
 
-instance.prototype.sendCmd = function(cmdStr) {
-	var self = this;
-	var cmd;
-	var end;
+	cmd = unescape(cmdStr)
+	end = '\n'
 
-	cmd = unescape(cmdStr);
-	end = '\n';
-	
-	console.log(cmd);
+	console.log(cmd)
 
-	/* 
+	/*
 	 * create a binary buffer pre-encoded 'latin1' (8bit no change bytes)
-	 * sending a string assumes 'utf8' encoding 
+	 * sending a string assumes 'utf8' encoding
 	 * which then escapes character values over 0x7F
 	 * and destroys the 'binary' content
 	 */
-	var sendBuf = Buffer.from(cmd + end, 'latin1');
-	
-	console.log(sendBuf);
+	var sendBuf = Buffer.from(cmd + end, 'latin1')
+
+	console.log(sendBuf)
 
 	if (sendBuf != '') {
 		// self.log('info','sending ',sendBuf,"to",self.config.host);
 
 		if (self.socket !== undefined && self.socket.connected) {
-			self.socket.send(sendBuf);
-		}
-		else {
-			self.log('error','Socket not connected :(');
+			self.socket.send(sendBuf)
+		} else {
+			self.log('error', 'Socket not connected :(')
 		}
 	}
 }
 
-instance_skel.extendedBy(instance);
-exports = module.exports = instance;
+instance_skel.extendedBy(instance)
+exports = module.exports = instance
